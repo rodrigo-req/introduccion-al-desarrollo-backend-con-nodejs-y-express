03d10003eb25658ecfe9ebc9b190fcc58b3a4002
rodrigo-req <rodrigosebas3@outlook.com>
2020-04-28 13:11:05 -0300

    Added async code to controller - without try-block

--- src/controllers/v1/users-controller.js
+++ src/controllers/v1/users-controller.js
@@ -1,21 +1,49 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
 const bcrypt = require('bcrypt');
 
-const createUser = (req, res) => {
-  console.log("req.body", req.body)
+// const createUser = (req, res) => {
+//   console.log('req.body', req.body);
 
-  
+//   // Necesito pasar un parametro password en el request.
+//   bcrypt.hash(req.body.password, 15, (err, hash) => {
+//     // Store hash in your password DB.
+//     if (err) {
+//       res.status(505).send({status: "Error", message: 'Invalid password!'});
+//       console.log('Error al generar contraseña!');
+//     } else {
+//       // Siendo el hash la password a almacenar.
+//       console.log('Contraseña generada:', hash);
+
+//       res.status(200).send({status: "Success", data: hash});
+//     }
+//   });
+// };
+
+/*
+  Convertimos la funcion en ASINCRONA, para que en caso de existir un error, no se intente mandar el hash como respuesta (ya que se envio un 505 anteriormente)
+*/
+const createUser = async (req, res) => {
+  console.log('req.body', req.body);
+
+  // Puedo guardar el hash en una variable, y AWAIT me permite acceder directamente al valor del string.
+  // En caso contrario, hash seria de tipo Promise<String>
+  const hash = await bcrypt.hash(req.body.password, 15);
+  console.log('FIN');
+
+  res.status(200).send({status: "Success", data: hash});
 };
 
 const deleteUser = (req, res) => {
-  res.send({status:"OK", message: "User deleted!"});
+  res.send({ status: 'OK', message: 'User deleted!' });
 };
 
 const getUsers = (req, res) => {
-  res.send({status:"OK", data: []});
+  res.send({ status: 'OK', data: [] });
 };
 
 const updateUser = (req, res) => {
-  res.send({status:"OK", message: "User updated!"});
+  res.send({ status: 'OK', message: 'User updated!' });
 };
 
 module.exports = {
